8c76be3f3d67da2ce5aba55e7d0ca393b9e1f658
Douglas Holman <holmando@oregonstate.edu>
2019-08-04 16:07:49 -0700

    solution to 204

--- leetcode_practice/8_4_2019.py
+++ leetcode_practice/8_4_2019.py
@@ -1,4 +1,6 @@
+import math
 from typing import List
+
 # 189. Rotate an array
 
 
@@ -26,3 +28,22 @@ class Solution:
             inplace_reversal(nums, 0, n - 1)
             inplace_reversal(nums, 0, k - 1)
             inplace_reversal(nums, k, n - 1)
+
+# 204. Count primes
+
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 3:
+            return 0
+
+        is_prime = [True] * n
+        is_prime[0] = False
+        is_prime[1] = False
+
+        for i in range(2, math.ceil(math.sqrt(n))):
+            if is_prime:
+                for j in range(i ** 2, n, i):
+                    is_prime[j] = False
+
+        return sum(is_prime)
