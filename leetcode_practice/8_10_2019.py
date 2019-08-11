c07cbbb960a22ce6fc4f38fbbc23b4051f5b8766
dholmancoding <holmando@oregonstate.edu>
2019-08-10 22:24:12 -0700

    28 complete

--- leetcode_practice/8_10_2019.py
+++ leetcode_practice/8_10_2019.py
@@ -13,3 +13,20 @@ class Solution:
             num_cts[number] += 1
 
         return min(num_cts, key=num_cts.get)
+
+# 28. Implement strStr()
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        len_needle = len(needle)
+        len_haystack = len(haystack)
+
+        if not needle:
+            return 0
+
+        for i, char in enumerate(haystack):
+            needle_candidate = haystack[i: min(i + len_needle, len_haystack)]
+            if needle_candidate == needle:
+                return i
+        return -1
